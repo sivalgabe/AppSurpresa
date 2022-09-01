781de9fe8b754d6f3ff26a6051bae959300b9fc3
sivalgabe <sival.gabriel@gmail.com>
2022-09-01 14:19:05 -0300

    Finalizar app

---
+++ app/src/main/java/com/example/igtim1tp1/ParabensActivity.kt
@@ -0,0 +1,11 @@
+package com.example.igtim1tp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ParabensActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parabens)
+    }
+}
